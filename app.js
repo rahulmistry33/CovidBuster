6a14c448af7323c166c4bf29ada35e45a048f4b5
rahulmistry33 <priyarahul0307@gmail.com>
2021-03-16 23:54:12 +0530

    Feat: Added blogging

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const news = require('./models/news');
+const news = require('./models/article');
 const bodyParser = require('body-parser');
-
+const methodOverride = require('method-override')
 const NewsAPI = require('newsapi');
+const article = require('./models/article');
 
 
+//CONNECT TO MONGODB
+mongoose.connect("mongodb://localhost:27017/covidBuster", { useUnifiedTopology: true },{ useNewUrlParser: true },{useCreateIndex: true}, {useFindAndModify: false})
 
-// DATABASE CONNECTION
-// RETURN A PROMISE
-// WHEN CONNECTION IS ESTABLISHED,ONLY THEN THE SERVER STARTS
 
 
 
@@ -25,7 +25,6 @@ const NewsAPI = require('newsapi');
  
 
 
-
 //REGISTERING VIEW ENGINE
 app.set('view engine', 'ejs');
 
@@ -34,7 +33,7 @@ app.use('/assets',express.static('assets'));
 app.use('/uploads',express.static('uploads'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'))
 // SETTING UP API-KEY LINK WITHT THE NEWS-API
 const newsapi = new NewsAPI('a8720d66af5749479e06c45ec5ff5a92');
 
@@ -69,6 +68,100 @@ app.get('/news', (req, res) => {
     
 });
 
+//ROUTE FOR BLOG PAGE
+app.get('/articles',(req,res) => {
+        
+        article.find({},(err,articles)=>{
+            if(err){
+                console.log("ERROR!");
+            }
+            else{
+                res.render('articles',{articles:articles})
+            }
+        })
+})
+
+app.get('/articles/:id',(req,res)=>{
+    console.log(req.url)
+    article.findById(req.params.id ,(err,articles)=>{
+        if(err) res.redirect('/articles')
+        else{
+            // console.log(articles);
+            res.render('show',{articles:articles})
+        }
+       
+    });
+    
+})
+
+app.get('/edit/:id',(req,res)=>{
+    console.log(req.url)
+    article.findById(req.params.id ,(err,articles)=>{
+        if(err) res.redirect('/articles')
+        else{
+            console.log(articles);
+            res.render('edit',{articles:articles})
+        }
+       
+    });
+    
+})
+
+app.put('/articles/:id',(req,res)=>{
+    console.log(req.url)
+    const items = new article({
+        title:req.body.title,
+        description:req.body.description,
+    });
+    items.save();
+    // articles.save()
+    article.findByIdAndDelete(req.params.id ,(err,articles)=>{
+        if(err){
+            console.log("Error, Article cannot be deleted")
+        }
+        else{
+            console.log("Updated the item")
+            res.redirect('/articles')
+        }
+       
+    });
+
+    
+    
+})
+
+
+app.delete('/articles/:id',(req,res)=>{
+    console.log(req.url)
+    article.findByIdAndDelete(req.params.id ,(err,articles)=>{
+        if(err){
+            console.log("Error, Article cannot be deleted")
+        }
+        else{
+            console.log("Deleted the item")
+            res.redirect('/articles')
+        }
+       
+    });
+    
+})
+
+app.get('/newArticle',(req,res) => {
+    res.render('createArticle')
+})
+
+app.post('/newArticle',(req,res)=>{
+    console.log("new article")
+    const articles = new article({
+        title:req.body.title,
+        description:req.body.description,
+    });
+    articles.save()
+    res.redirect('/articles')
+})
+
+
+
 //ROUTE FOR LOGIN PAGE
 app.get('/Login', (req, res) => {
     console.log("Redirected to login page");
@@ -91,8 +184,8 @@ app.use((req, res) => {
 
 
 //SETTING UP THE PORT
-app.listen(PORT, () => {
-	console.log(`Server started listening on port ${PORT}`);
+app.listen(3000, () => {
+	console.log("Server started listening on port 3000");
 });
 
 
