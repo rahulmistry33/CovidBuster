cab6188e32ab4255a5770d3cdc5f8a50f018ec75
Prachit99 <prachitmhalgi@gmail.com>
2021-04-08 22:30:02 +0530

    login register & navbar css

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { DisplayArticleComponent } from './components/display-article/display-ar
 import { EditComponentComponent } from './components/edit-component/edit-component.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 import { MapsComponent } from './components/maps/maps.component';
 import { NewsComponent } from './components/news/news.component';
 
@@ -20,6 +21,7 @@ const routes: Routes = [
   {path:'news',component:NewsComponent},
   {path:'maps',component:MapsComponent},
   {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
 ];
 
 @NgModule({
