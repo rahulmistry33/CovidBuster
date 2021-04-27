3d7907793a08a6109d153066654c14df87ea3eb2
rahulmistry33 <priyarahul0307@gmail.com>
2021-04-27 19:40:39 +0530

    Added MongoDB atlas connection

--- models/db.js
+++ models/db.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
+const uri = 'mongodb+srv://rahulmistry:1234@cluster0.k9erx.mongodb.net/covid-buster?retryWrites=true&w=majority'
 
-mongoose.connect('mongodb://localhost:27017/covidBuster', {
+// mongoose.connect('mongodb://localhost:27017/covidBuster', {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+// })
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
